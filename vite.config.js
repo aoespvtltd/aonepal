3de05a5918409d9f79bbecc13e4e3757e0dc1b26
aoespvtltd <aoespvtltd@gmail.com>
2024-12-30 15:06:12 +0545

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touc-icon.png", "masked-icon.png"],
+  manifest: {
+    name: "",
+    short_name: "",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+  },
+};
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestForPlugin)],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
